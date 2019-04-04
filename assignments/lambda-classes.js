cd06cde6419858c370dfde03859e15efc121d81b
Moses Samuel <mosamuel98@gmail.com>
2019-04-04 14:20:41 -0700

    add Student class + refactor all methods to return its caller

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,29 +1,58 @@
 class Person {
-    constructor(attributes) {
-        this.name = attributes.name
-        this.age = attributes.age
-        this.location = attributes.location
-        this.gender = attributes.location
+    constructor(traits) {
+        this.name = traits.name
+        this.age = traits.age
+        this.location = traits.location
+        this.gender = traits.location
     }
 
     speak() {
-        return `Hello my name is ${this.name}, I am from ${this.location}`
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
+        return this
     }
 }
 
 class Instructor extends Person {
-    constructor(attributes) {
-        super(attributes)
-        this.specialty = attributes.specialty
-        this.favLanguage = attributes.favLanguage
-        this.catchPhrase = attributes.catchPhrase
+    constructor(traits) {
+        super(traits)
+        this.specialty = traits.specialty
+        this.favLanguage = traits.favLanguage
+        this.catchPhrase = traits.catchPhrase
     }
 
     demo(subject) {
-        return `Today we are learning about ${subject}'`
+        console.log(`Today we are learning about ${subject}`)
+        return this
     }
 
     grade(student, subject) {
-        return ` ${student.name} receives a perfect score on ${subject}`
+        console.log(` ${student.name} receives a perfect score on ${subject}`)
+        return this
+    }
+}
+
+class Student extends Person {
+    constructor(traits) {
+        super(traits) 
+        this.previousBackground = traits.previousBackground
+        this.className = traits.className
+        this.favSubjects = traits.favSubjects
+    }
+
+    listsSubjects() {
+        this.favSubjects.forEach(subject => { 
+            console.log(subject)
+            return this
+        })
+    }
+
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`)
+        return this
+    }
+
+    sprintChallenge() {
+        console.log(`${this.name} has begun sprint challenge on ${subject}`)
+        return this
     }
 }
